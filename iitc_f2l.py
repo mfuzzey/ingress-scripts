744d81e03cf603ba65d6afa1908d99a6e7e44426
Martin Fuzzey <mfuzzey@gmail.com>
2015-04-04 16:19:08 +0200

    add option to remove markers

--- iitc_f2l.py
+++ iitc_f2l.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import unittest
 
@@ -33,13 +34,21 @@ def normalize(objs):
 	return "[%s]" % ",".join(out)
 
 	
-def f2l(src):
+def f2l(src, args):
 	def buildLink(a, b, color):
 		return {"type" : "polyline", "latLngs": [a, b], "color":color}
 		
+	skipTypes = []
+	if args.remove_markers:
+		skipTypes.append("marker")
+		
 	dest = []
 	for obj in eval(src):
-		if obj["type"] == "polygon":
+		objType = obj["type"]
+		if objType in skipTypes:
+			continue
+			
+		if objType == "polygon":
 			vertices = obj["latLngs"]
 			color = obj["color"]
 			dest.append(buildLink(vertices[0], vertices[1], color))
@@ -50,39 +59,52 @@ def f2l(src):
 	return normalize(dest)
 	
 
+def buildOptionParser():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--no-markers", dest="remove_markers", action="store_true", help="remove any markers")
+	return parser
+
+
 class Autotest(unittest.TestCase):
 	def test01_alreadyLine(self):
 		alreadyOk = '[{"type":"polyline","latLngs":[{"lat":47.264051,"lng":6.052921},{"lat":47.761173,"lng":7.294874}],"color":"#a24ac3"},{"type":"polyline","latLngs":[{"lat":47.761173,"lng":7.294874},{"lat":48.461385,"lng":6.320278}],"color":"#a24ac3"}]'
-		self.assertEquals(f2l(alreadyOk), alreadyOk)
+		self.assertEquals(f2l(alreadyOk, self._mkArgs()), alreadyOk)
 
 	def test02_reorderLine(self):
 		inp = '[{"latLngs":[{"lat":47.264051,"lng":6.052921},{"lat":47.761173,"lng":7.294874}],"color":"#a24ac3", "type":"polyline"},{"type":"polyline","latLngs":[{"lng":7.294874, "lat":47.761173},{"lat":48.461385,"lng":6.320278}],"color":"#a24ac3"}]'
 		exp = '[{"type":"polyline","latLngs":[{"lat":47.264051,"lng":6.052921},{"lat":47.761173,"lng":7.294874}],"color":"#a24ac3"},{"type":"polyline","latLngs":[{"lat":47.761173,"lng":7.294874},{"lat":48.461385,"lng":6.320278}],"color":"#a24ac3"}]'
-		self.assertEquals(f2l(inp), exp)
+		self.assertEquals(f2l(inp, self._mkArgs()), exp)
 
 	def test03_fieldToLink(self):
 		inp = '[{"type":"polygon","latLngs":[{"lat":47.264051,"lng":6.052921},{"lat":47.761173,"lng":7.294874},{"lat":48.461385,"lng":6.320278}],"color":"#a24ac3"}]'
 		exp ='[{"type":"polyline","latLngs":[{"lat":47.264051,"lng":6.052921},{"lat":47.761173,"lng":7.294874}],"color":"#a24ac3"},{"type":"polyline","latLngs":[{"lat":47.264051,"lng":6.052921},{"lat":48.461385,"lng":6.320278}],"color":"#a24ac3"},{"type":"polyline","latLngs":[{"lat":47.761173,"lng":7.294874},{"lat":48.461385,"lng":6.320278}],"color":"#a24ac3"}]'
-		self.assertEquals(f2l(inp), exp)
+		self.assertEquals(f2l(inp, self._mkArgs()), exp)
 
 	def test04_marker(self):
 		inp = '[{"type":"marker","latLng":{"lat":49.601164,"lng":7.376891},"color":"#c34a6f"}]'
 		exp = '[{"type":"marker","latLng":{"lat":49.601164,"lng":7.376891},"color":"#c34a6f"}]'
-		self.assertEquals(f2l(inp), exp)
+		self.assertEquals(f2l(inp, self._mkArgs()), exp)
 
+	def test05_removeMarker(self):
+		inp = '[{"type":"marker","latLng":{"lat":49.601164,"lng":7.376891},"color":"#c34a6f"},{"type":"polyline","latLngs":[{"lat":47.264051,"lng":6.052921},{"lat":47.761173,"lng":7.294874}],"color":"#a24ac3"},{"type":"polyline","latLngs":[{"lat":47.761173,"lng":7.294874},{"lat":48.461385,"lng":6.320278}],"color":"#a24ac3"}]'
+		exp = '[{"type":"polyline","latLngs":[{"lat":47.264051,"lng":6.052921},{"lat":47.761173,"lng":7.294874}],"color":"#a24ac3"},{"type":"polyline","latLngs":[{"lat":47.761173,"lng":7.294874},{"lat":48.461385,"lng":6.320278}],"color":"#a24ac3"}]'
+		self.assertEquals(f2l(inp, self._mkArgs("--no-markers")), exp)
+
+	def _mkArgs(self, options=""):
+		parser = buildOptionParser()
+		return parser.parse_args(options.split())
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		print "Test Mode  (please supply input file for real mode"
 		unittest.main()
+		
+	parser = buildOptionParser()	
+	parser.add_argument("infile", type=argparse.FileType("rt"), help="Input file in IITC format")
+	parser.add_argument("outfile", type=argparse.FileType("wt"), nargs="?", default="-", help="Output file")
 	
-	inp = open(sys.argv[1], "rt").read()
-	res = f2l(inp)
-	if len(sys.argv) > 2:
-		outfile = sys.argv[2]
-		out = open(outfile, "wt")
-		out.write(res)
-		print "Written result to %s" % outfile
-	else:
-		print res
+	args = parser.parse_args()
+
+	res = f2l(args.infile.read(), args)
+	args.outfile.write(res)
 
